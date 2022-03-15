4c02e1d92441106bafd32947f20404363c7b50da
Pavithra <pavithra@alphabsolutions.com>
2022-03-15 15:30:31 +0530

    Inline Style

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styles: [
+    '.custom{color:red}'
+  ]
+})
+export class CustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
