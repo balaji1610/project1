397c1aa06806e75f25fa946c904f8110cd9cdb99
Pavithra <pavithra@alphabsolutions.com>
2022-03-15 13:13:20 +0530

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,28 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'project1';
   name = "balaji";
-}
+
+  getvalue(){  // Interpolation Databinding
+
+    return 'Sun Rise'
+  }
+  num=15;  // calculation
+
+  newClass='class01'  // binding assign class
+
+  ifCondition(){
+
+    var votingage = 18;
+
+    if(votingage>=18){
+
+      return "You Eligible For Vote"
+    }else{
+      return "Your Not Eligble For"
+    }
+  }
+
+
+
+
+}// End
